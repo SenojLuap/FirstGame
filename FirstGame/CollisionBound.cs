17f9a280573c2b3de45c361089dbb06a15b70554
Paul Jones <shirlypunk@hotmail.com>
2015-11-28 23:12:12 -0500

    Beginning transition to OOP paradigm

---
+++ FirstGame/CollisionBound.cs
@@ -0,0 +1,60 @@
+﻿using paujo.GameUtility;
+
+using Microsoft.Xna.Framework;
+
+namespace paujo.FirstGame {
+  public interface ICollisionBound {
+    bool CollidesWith(ICollisionBound otherBound);
+  }
+
+  
+  public class CircleBound : ICollisionBound {
+
+    public Circle Circle {
+      get; set;
+    }
+
+    
+    public CircleBound(Circle circle) {
+      this.Circle = circle;
+    }
+    
+    public bool CollidesWith(ICollisionBound otherBound) {
+      CircleBound otherCircle = otherBound as CircleBound;
+      if (otherCircle != null) {
+	return Collision.Collides(Circle, otherCircle.Circle);
+      }
+      RectBound otherRect = otherBound as RectBound;
+      if (otherRect != null) {
+	return Collision.Collides(otherRect.Rect, Circle);
+      }
+      return false;
+    }
+  }
+
+
+  public class RectBound : ICollisionBound {
+    
+    public Rectangle Rect {
+      get; set;
+    }
+
+
+    public RectBound(Rectangle rect) {
+      this.Rect = rect;
+    }
+
+
+    public bool CollidesWith(ICollisionBound otherBound) {
+      RectBound otherRect = otherBound as RectBound;
+      if (otherRect != null) {
+	return Collision.Collides(Rect, otherRect.Rect);
+      }
+      CircleBound otherCircle = otherBound as CircleBound;
+      if (otherCircle != null) {
+	return Collision.Collides(Rect, otherCircle.Circle);
+      }
+      return false;
+    }
+  }
+}
---
+++ FirstGame/WorldEntity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using paujo.GameUtility;
+
+namespace paujo.FirstGame {
+  public abstract class WorldEntity {
+
+    public Point Pos {
+      get; set;
+    }
+    
+
+    abstract public void GameTick(double deltaTime);
+
+
+    abstract public void Draw(FirstGame game, double deltaTime);
+
+
+    public ICollisionBound GetFastBound() {
+      return new CircleBound(new Circle(Pos, 1));;
+    }
+
+    
+    public List<ICollisionBound> GetAccurateBound() {
+      List<ICollisionBound> res = new List<ICollisionBound>();
+      res.Add(GetFastBound());
+      return res;
+    }
+  }
+}
