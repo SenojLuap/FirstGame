a8c8fe32a4a543258faec77694323e57f103872d
Paul Jones <shirlypunk@hotmail.com>
2015-12-03 17:09:53 -0500

    Created player class

---
+++ FirstGame/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace paujo.FirstGame {
+  public class Player : MovingEntity {
+
+    /**
+     * World Entity
+     */
+    override public void GameTick(FirstGame game, double deltaTime) {
+      Move(deltaTime);
+    }
+    
+  }
+}
